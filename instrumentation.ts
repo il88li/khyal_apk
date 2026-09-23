98d220f74223a1760bc326aa4a18477e099fd58e
il88li <yl9667383@gmail.com>
2026-09-23 09:31:36 +0300

    Create instrumentation.ts

---
+++ instrumentation.ts
@@ -0,0 +1,17 @@
+export async function register() {
+  if (process.env.NEXT_RUNTIME === 'nodejs') {
+    await import('./sentry.server.config');
+  }
+  if (process.env.NEXT_RUNTIME === 'edge') {
+    await import('./sentry.edge.config');
+  }
+}
+
+export async function onRequestError(
+  err: unknown,
+  request: { path: string; method: string; headers: Record<string, string> },
+  context: { routerKind: string; routePath: string; routeType: string }
+) {
+  const Sentry = await import('@sentry/nextjs');
+  Sentry.captureRequestError(err, request, context);
+}
